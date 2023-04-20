272516762111b4bb6b71a1e087bd56325a42c7b9
Evans Wanyoike <evanswanyoi@gmail.com>
2023-04-20 10:25:59 +0300

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Evans Wanyoike on 20/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
